5d69ca44e895b6febd1c2aed4952185696bf69d7
cmy <312564686@qq.com>
2020-06-24 16:02:33 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -6,11 +6,28 @@
 [es](https://elasticsearch.cn/explore)
 [Bootstrap](https://v3.bootcss.com/getting-started/)
 [Github OAuth](https://developer.github.com/apps/building-oauth-apps/)
-
+[spring boot](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 ## Tools
 Git
 [Visual Paradigm](https://www.visual-paradigm.com)
 
-##Day01
+
+## script
+```sql
+CREATE CACHED TABLE "PUBLIC"."USER"(
+    "ID" INT DEFAULT NEXT VALUE FOR "PUBLIC"."SYSTEM_SEQUENCE_CCB2A368_D475_4391_8568_061EE41918E1" NOT NULL NULL_TO_DEFAULT SEQUENCE "PUBLIC"."SYSTEM_SEQUENCE_CCB2A368_D475_4391_8568_061EE41918E1",
+    "ACCOUNT_ID" VARCHAR(100),
+    "NAME" VARCHAR(50),
+    "TOKEN" VARCHAR(36),
+    "GMT_CREATE" BIGINT,
+    "GMT_MODIFIED" BIGINT
+)
+```
+
+## Day01
 Bug1: Can't connect to api.github.com when in `githubProvider.getUser();`.
 [Soluiton](https://niter.cn/p/125)
+
+## Day02
+
+
--- src/main/java/org/cmy/community/provider/GithubProvider.java
+++ src/main/java/org/cmy/community/provider/GithubProvider.java
@@ -2,16 +2,6 @@ package org.cmy.community.provider;
 
 import com.alibaba.fastjson.JSON;
 import okhttp3.*;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpVersion;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 import org.cmy.community.dto.AccessTokenDTO;
 import org.cmy.community.dto.GithubUser;
 import org.springframework.stereotype.Component;
@@ -32,48 +22,13 @@ public class GithubProvider {
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
-//            System.out.println(string);
             String token = string.split("&")[0].split("=")[1];
-//            System.out.println(token);
             return token;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-//    public GithubUser getUser(String accessToken) throws Exception {
-//        int timeout = 60;
-//        RequestConfig defaultRequestConfig = RequestConfig.custom()
-//                .setSocketTimeout(timeout * 1000)
-//                .setConnectTimeout(timeout * 1000)
-//                .setConnectionRequestTimeout(timeout * 1000)
-//                .build();
-//        CloseableHttpClient httpclient = HttpClients.createDefault();
-//        HttpGet httpget = new HttpGet("https://api.github.com/user");
-//        httpget.addHeader(new BasicHeader("Authorization", "token "+accessToken));
-//        httpget.setProtocolVersion(HttpVersion.HTTP_1_0);
-//        httpget.addHeader(HTTP.CONN_DIRECTIVE, HTTP.CONN_CLOSE);
-//        httpget.setConfig(defaultRequestConfig);
-//        CloseableHttpResponse response = null;
-//        try {
-//            response = httpclient.execute(httpget);
-//            System.out.println(response.getStatusLine());
-//            HttpEntity entity = response.getEntity();
-//            GithubUser user = JSON.parseObject(entity.getContent(),GithubUser.class);
-//            EntityUtils.consume(entity);
-//            response.close();
-//            return  user;
-//        } catch (Exception e){
-//            throw e;
-//        } finally{
-//            if(response != null){
-//                response.close();
-//            }
-//            if(httpclient!=null){
-//                httpclient.close();
-//            }
-//        }
-//    }
 
     public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
@@ -82,7 +37,6 @@ public class GithubProvider {
                 .url("https://api.github.com/user")
                 .header("Authorization","token "+accessToken)
                 .build();
-
         try {
             Response response = client.newCall(request).execute();
             String string = response.body().string();
