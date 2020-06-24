b6672f68ec265949d90632d1a2f9863fcd41e068
cmy <312564686@qq.com>
2020-06-24 19:45:54 +0800

    add check cookies

--- src/main/java/org/cmy/community/mapper/UserMapper.java
+++ src/main/java/org/cmy/community/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package org.cmy.community.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.cmy.community.model.User;
 
 @Mapper
@@ -9,4 +11,6 @@ public interface UserMapper {
     @Insert("insert into user (name, account_id, token, gmt_create, gmt_modified) values(#{name}, #{accountId}, #{token}," +
             " #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token = #{token}") //在mybatis中，#{}会将函数中的形参放入，形参不是类的话需要加@Param
+    User findByToken(@Param("token") String token);
 }
