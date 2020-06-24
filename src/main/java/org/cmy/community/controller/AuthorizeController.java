26ba0c64d4a577ac82daa38025ec9e5c5c735c81
cmy <312564686@qq.com>
2020-06-24 13:38:25 +0800

    extract github config from code to properties

--- src/main/java/org/cmy/community/controller/AuthorizeController.java
+++ src/main/java/org/cmy/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import org.cmy.community.dto.AccessTokenDTO;
 import org.cmy.community.dto.GithubUser;
 import org.cmy.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +15,25 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    //当spring容器启动时会去读取配置文件（key=value形式），并放到map中。
+    // 此处调用该Value注解，会从map中获取github.client.id对应的value值。
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state") String state) throws Exception {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("b39a8e8b8f2661e83753");
-        accessTokenDTO.setClient_secret("6ad18343ba0d10884346b3bcebeddb603a350ad5");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        System.out.println(accessToken);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
         return "index";
